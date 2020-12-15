9af62051011d0eb97641e12b766c109df1e554f4
彭皓 <hao.peng01@hand-china.com>
2020-12-15 09:57:15 +0800

    12.15

--- src/main/java/LeetCode.java
+++ src/main/java/LeetCode.java
@@ -854,6 +854,23 @@ public class LeetCode {
         root.right = temp;
     }
 
+    public int monotoneIncreasingDigits(int N) {
+        char[] str = Integer.toString(N).toCharArray();
+        int i = 1;
+        while (i < str.length && str[i - 1] <= str[i]) {
+            i++;
+        }
+        if (i < str.length) {
+            while (i > 0 && str[i - 1] > str[i]) {
+                str[i - 1]--;
+                i--;
+            }
+            for (i++; i < str.length; i++) {
+                str[i] = '9';
+            }
+        }
+        return Integer.parseInt(new String(str));
+    }
 
     public int minCostClimbingStairs2(int[] cost) {
         int len = cost.length;
