a716f78faa2744dcd0173c24667086838c5ee0f0
彭皓 <hao.peng01@hand-china.com>
2020-12-31 15:30:08 +0800

    12.31

--- src/main/java/LeetCode.java
+++ src/main/java/LeetCode.java
@@ -1231,6 +1231,30 @@ public class LeetCode {
     }
 
 
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] interval1, int[] interval2) {
+                return interval1[1] - interval2[1];
+            }
+        });
+
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            if (intervals[i][0] >= right) {
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+
+
     public int minCostClimbingStairs3(int[] cost) {
         int f1 = 0, f2 = 0;
         for (int i = 0; i < cost.length; i++) {
